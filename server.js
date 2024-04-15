dcf779f4ffa43c0eec435ab5c9b471409476942c
Alpha John <alphajohn1505@gmail.com>
2024-04-15 13:00:49 +0530

    dotenv

--- server.js
+++ server.js
@@ -4,21 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
+
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+app.use(cors()); // Enable CORS for all origins
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
@@ -33,6 +22,5 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
